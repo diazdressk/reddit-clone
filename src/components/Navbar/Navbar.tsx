e7288e25b63d34a5d4c7d8778b2a67150da807f1
@altpen1993 <a.abdurasulov@takeoff-staff.ru>
2022-06-03 02:02:46 +0600

    Create Community done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,12 +4,12 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase/clientApp';
 import RightContent from './RightContent/RightContent';
 import SearchInput from './SearchInput';
-
+import Directory from './Directory/Directory'
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth)
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
+    <Flex bg="white" height="44px" padding="6px 12px" justify={{ md: 'space-between' }}>
+      <Flex align="center" width={{ base: "none", md: 'auto' }} mr={{ base: 0, md: 2 }}>
         <Image src="/images/redditFace.svg" height="30px" mr={[2, 0, 0, 0]} /*responsive margin ['sm', 'md', 'lg', 'xl']*/ />
         <Image
           src="/images/redditText.svg"
@@ -20,9 +20,9 @@ const Navbar: React.FC = () => {
           }}
         />
       </Flex>
-      <SearchInput />
+      {user && <Directory />}
+      <SearchInput user={user} />
       <RightContent user={user} />
-      {/* <Directory /> */}
     </Flex>
   );
 };
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,26 +1,40 @@
 import { SearchIcon } from '@chakra-ui/icons';
 import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { User } from 'firebase/auth';
 import React from 'react';
 
-type SearchInputProps = {};
+type SearchInputProps = {
+  user?: User | null;
+};
 
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1}/*????, take all remaining width of parents container */ mr={2} align="center">
+    <Flex
+      flexGrow={1}
+      /*????, take all remaining width of parents container */ mr={2}
+      align="center"
+      maxWidth={user ? 'auto' : '600px'}>
       <InputGroup>
-        <InputLeftElement pointerEvents="none"/*???*/ children={<SearchIcon color="gray.400" mb={1} />} />
+        <InputLeftElement
+          pointerEvents="none"
+          /*???*/ children={<SearchIcon color="gray.400" mb={1} />}
+        />
         <Input
           placeholder="Search Reddit"
           fontSize="10pt"
           _placeholder /*_ styles for placeholder*/={{ color: 'gray.500' }}
-          /*_ styles on hover*/ _hover={{ bg: 'white', border: '1px solid', borderColor: 'blue.500' }}
+          /*_ styles on hover*/ _hover={{
+            bg: 'white',
+            border: '1px solid',
+            borderColor: 'blue.500',
+          }}
           _focus={{
             outline: 'none',
-            border: "1px solid",
-            borderColor: "blue.500"
+            border: '1px solid',
+            borderColor: 'blue.500',
           }}
-          height='34px'
-          bg='gray.50'
+          height="34px"
+          bg="gray.50"
         />
       </InputGroup>
     </Flex>
